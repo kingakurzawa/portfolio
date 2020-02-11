9fd8f7f78dbb987b364587395d02eede62debe48
kingakurzawa <j.kingakurzawa@gmail.com>
2020-02-11 23:41:43 +0000

    created section projects, contact and footer

--- js/main.js
+++ js/main.js
@@ -1,31 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-  $(".card").slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+  $(".project-carousel").slick({
+    autoplay: false,
+    autoplaySpeed: 3500,
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
+        breakpoint: 760,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 992,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 3,
           slidesToScroll: 1
         }
       }
